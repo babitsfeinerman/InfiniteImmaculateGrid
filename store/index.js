43e56f86fbb655d69e6c0f8103f35cb95aa3634e
babitsfeinerman <98004803+babitsfeinerman@users.noreply.github.com>
2023-08-07 23:41:44 -0400

    Update index.js
    added comment

--- store/index.js
+++ store/index.js
@@ -1,16 +1,19 @@
+//imports Vue and Vuex, and then tells Vue to use Vuex as a plugin
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
 const createStore = () => {
-  return new Vuex.Store({
-    state: {
+  return new Vuex.Store({ //defines a function named createStore that returns a new Vuex store instance
+    state: {// The state object holds the application's centralized state. In this case, 
+      //it includes properties for selectedGridLocation, selectedPlayer, and grid.
       selectedGridLocation: null,
       selectedPlayer: null,
       grid: null
-    },
-    mutations: {
+    }, 
+    mutations: { //Mutations are functions that directly modify the state. Each mutation receives the state as its first argument.
       setSelectedGridLocation (state, item) {
         state.selectedGridLocation = item
       },
@@ -28,4 +31,9 @@ const createStore = () => {
   })
 }
 
-export default createStore
+export default createStore //The createStore function is exported as the default export. 
+//This allows the store to be imported and used in other parts of the Vue application.
+
+//this code sets up a Vuex store with state properties (selectedGridLocation, selectedPlayer, grid) 
+//and mutation functions to modify these state properties.
+
